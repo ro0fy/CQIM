b7919b76e8a92a5b0bff083ff66236541852149d
Kirill Gan <103137118+ro0fy@users.noreply.github.com>
2022-06-13 17:21:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,25 +11,11 @@ if not os.path.exists("./drives"):
 if not os.path.exists("./machines"):
 	os.mkdir("./machines")
 
-if os.path.exists("./qemu-path"):
-    os.system("python3 ./main.py")
-
-else:
+eif not os.path.exists(".qemu-path"):
 	qemu_path = input("Path to qemu binaries\n>>")
 	qemu_path_file = open("qemu-path","w+")
 	qemu_path_file.write(qemu_path)
 	qemu_path_file.close
-
-os.system("python3 ./main.py")
-    
-# -*- coding: utf-8 -*-
-#Copyright Gan Kirill 2022
-
-#This file is part of CQIM.
-#CQIM is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-#CQIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License along with CQIM. If not, see <https://www.gnu.org/licenses/>. 
-
 
 import os
 
